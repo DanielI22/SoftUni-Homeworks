2fe8f13adb63e40cae9058f831cfa5ab0cb83656
Daniel Ivanov <80488757+DanielI22@users.noreply.github.com>
2022-07-24 23:05:01 +0300

    Reflection&Attributes

---
+++ Advanced/ReflectionAndAttributes/CommandPattern/Core/Commands/CommandInterpreter.cs
@@ -0,0 +1,31 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CommandPattern.Core.Commands
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] tokens = args.Split(' ');
+            string command = tokens[0];
+            string[] comArgs = tokens.Skip(1).ToArray();
+
+            Assembly assembly = Assembly.GetEntryAssembly();
+            Type cmdType = assembly.GetTypes().FirstOrDefault(cmdType => cmdType.Name == $"{command}Command");
+
+            if(cmdType == null)
+            {
+                throw new ArgumentException("Invalid Type!");
+            }
+
+            ICommand commandInstance = (ICommand)Activator.CreateInstance(cmdType);
+
+            return commandInstance.Execute(comArgs);
+        }
+    }
+}
---
+++ Advanced/ReflectionAndAttributes/ValidationAttributes/Validator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ValidationAttributes
+{
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            Type type = obj.GetType();
+            PropertyInfo[] properties = type.GetProperties()
+                .Where(pi => pi.CustomAttributes.Any(a => a.AttributeType.BaseType == typeof(MyValidationAttribute))).ToArray();
+
+
+            foreach(PropertyInfo property in properties)
+            {
+                object propValue = property.GetValue(obj);
+
+                foreach(CustomAttributeData customAttributeData in property.CustomAttributes)
+                {
+                    Type customAttr = customAttributeData.AttributeType;
+                    object attrInstance = property.GetCustomAttribute(customAttr);
+
+                    MethodInfo velidationMethod = customAttr.GetMethods().First(m => m.Name == "IsValid");
+                    bool result = (bool)velidationMethod.Invoke(attrInstance, new object[] { propValue });
+
+                    if(!result)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
